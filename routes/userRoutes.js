7cec6fc04e49de8adac72d740912f2a608b27eb2
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-02-17 11:08:01 +0530

    register user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,2 +1,10 @@
+import express from "express";
 import axios , {all} from "axios";
 import dotenv from "dotenv";
+import { newUser } from "../controllers/userController.js";
+
+const userRoutes = express.Router();
+
+userRoutes.post("/register", newUser);
+
+export default userRoutes;
