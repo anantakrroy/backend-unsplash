7a80655fbb6b87263b3305f5fcd1eac412229286
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-02-17 11:06:17 +0530

    User controller added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,35 @@
 //Import asyncHandler so that we can use it in our routes to trigger error handling middleware
-const asyncHandler = require("express-async-handler");
+import asyncHandler from "express-async-handler";
+import bcrypt from "bcryptjs";
+import { register } from "../models/userModel.js";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const saltRounds = process.env.SALT_ROUNDS;
+const pepper = process.env.PEPPER;
+
+export const newUser = async (req, res, next) => {
+    try {
+        console.log("Called user register controller....");
+        const hashedPassword = bcrypt.hashSync(req.body.password + pepper, parseInt(saltRounds));
+        const user = {
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+        }
+        // console.log(`User in request : ${username} ${email} ${password}`); 
+        const modelresponse = await register(user);
+        // console.log(`Model response --> ${modelresponse}`);
+        
+        res.send(modelresponse);
+    } catch (error) {
+        next(error);
+        // const err = error
+        // res.status(400).json({
+        //     "message" : error
+        // });
+        // console.log(`Controller error ..>`, error);
+        // next(error);
+    }
+}
