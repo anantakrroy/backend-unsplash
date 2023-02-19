eb15329dcc263d4eef5bbbf6ac5b207300cd4c37
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-02-20 00:42:12 +0530

    Added more user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import axios , {all} from "axios";
 import dotenv from "dotenv";
-import { newUser } from "../controllers/userController.js";
+import { loginUser, logoutUser, newUser, userInfo } from "../controllers/userController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const userRoutes = express.Router();
 
 userRoutes.post("/register", newUser);
-
+userRoutes.post("/login", loginUser);
+userRoutes.post("/logout", logoutUser);
+userRoutes.get("/me",authMiddleware, userInfo);
 export default userRoutes;
