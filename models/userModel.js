0075c40ae8b89ded67eded515f1830e2445ec6f8
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-02-20 00:41:55 +0530

    Added login model

--- models/userModel.js
+++ models/userModel.js
@@ -4,14 +4,24 @@ import mongoose from "mongoose";
 
 const UserModel = mongoose.model('User', userSchema, "users");
 
-  
-
-//  Register a new user
+//  Register model
 export const register = async (user) => {
     try {
         const newUser = new UserModel({...user, timestamp: Date.now()});
-        return await newUser.save()
+        return await newUser.save();
     } catch (error) {
         throw new Error(JSON.stringify(error));
+    }
+}
+
+// Login model
+export const login = async(user) => {
+    console.log('user login model called........')
+    try {
+        // const query = await UserModel.where({username: user.email});
+        const findUser = await UserModel.findOne({email: user.email});
+        return findUser;
+    } catch (error) {
+        throw new Error(error);
     }
 }
