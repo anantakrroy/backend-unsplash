e5cc1ab874beed419710483344a015a621365d70
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-02-20 19:20:41 +0530

    Refactor add favorite model

--- models/favoritePhotoModel.js
+++ models/favoritePhotoModel.js
@@ -1,22 +1,33 @@
-import { favoritePhotosSchema } from "../config/schema.js";
+import { favoritePhotosSchema, userSchema } from "../config/schema.js";
 import mongoose from "mongoose";
 import database from "../config/db.js";
 
 const favPhotosModel = mongoose.model('Favorites', favoritePhotosSchema, "favoritePhotos");
+const userModel = mongoose.model('User', userSchema, "users");
 
-export const addFavorites = async (photo) => {
+
+export const addFavorites = async (photo, username) => {
     try {
-        console.log(`Photo to be added to fav ---> ${Object.entries(photo)}`);
+        // console.log(`Photo to be added to fav ---> ${Object.entries(photo)} for the user ----> ${username}......`);
         const favPhoto = new favPhotosModel(photo);
-        const favExists = await favPhotosModel.findOne({url: photo.url}) || null;
+        const userExists = await userModel.findOne({ username: username }) || null;
+        const favExists = await favPhotosModel.findOne({ url: photo.url }) || null;
         console.log('Is photo already fav ? ---->', favExists)
-        if(!favExists)
-            await favPhoto.save();
-        else
+        if (userExists) {
+            if (!favExists)
+                await favPhoto.save();
+            else
+                throw {
+                    "message": "Already favorited !",
+                    "name": "ReferenceError"
+                }
+        } else {
             throw {
-                "message": "Already favorited !",
-                "name" : "ReferenceError"
+                "name" : "ReferenceError",
+                "message" : "No user found !",
+                "statusCode" : 400
             }
+        }
     } catch (error) {
         console.log(`favorite add model error ----> ${error}`);
         throw error;
