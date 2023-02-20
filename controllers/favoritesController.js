b403168c8e8fb99e70d653924d4cb739a2078995
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-02-20 20:38:15 +0530

    Add controller for getting fav pics

--- controllers/favoritesController.js
+++ controllers/favoritesController.js
@@ -1,7 +1,7 @@
 //Import asyncHandler so that we can use it in our routes to trigger error handling middleware
 import asyncHandler from "express-async-handler";
 import express from "express";
-import { addFavorites } from "../models/favoritePhotoModel.js";
+import { addFavorites, getFavoritePhotos } from "../models/favoritePhotoModel.js";
 
 
 
@@ -9,18 +9,17 @@ import { addFavorites } from "../models/favoritePhotoModel.js";
 export const addToFavorites = asyncHandler(async (req, res, next) => {
     console.log("Add to favs controller called ......");
     const username = req.params.username;
-    const userLoggingIn = req.data.username;
-    if(username !== userLoggingIn) {
-        throw {
-            "name" : "AuthorizationError",
-            "message" : "Unauthenticated user ! You can add photos to your own collection !",
-            "statusCode" : 401
-        }
-    }
     const favPhoto = req.body;
     const data = await addFavorites(favPhoto, username);
     res.status(200).json({
         "message" : "Photo added to favorites !",
         "collection" : data
     });
+})
+
+// Get favorites
+export const getFavorites = asyncHandler(async(req, res, next) => {
+    const username = req.params.username;
+    const favPhotos = await getFavoritePhotos(username);
+    res.status(200).send(favPhotos);
 })
