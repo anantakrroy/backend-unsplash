d04c96d077149ceb17254bb8f8bbcf546d850624
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-02-20 18:15:37 +0530

    Favorites controller

--- controllers/favoritesController.js
+++ controllers/favoritesController.js
@@ -1,2 +1,17 @@
 //Import asyncHandler so that we can use it in our routes to trigger error handling middleware
-const asyncHandler = require("express-async-handler");
+import asyncHandler from "express-async-handler";
+import express from "express";
+import { addFavorites } from "../models/favoritePhotoModel.js";
+
+
+
+// Add to favorites collection
+export const addToFavorites = asyncHandler(async (req, res, next) => {
+    console.log("Add to favs controller called ......");
+    const favPhoto = req.body;
+    const data = await addFavorites(favPhoto);
+    res.status(200).json({
+        "message" : "Photo added to favorites !",
+        "collection" : data
+    });
+})
