1e581a5c9617cc49c25303e400c8858e141dfd9b
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-02-20 20:38:43 +0530

    Added route for get fav pics

--- routes/favoritesRoutes.js
+++ routes/favoritesRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { addToFavorites } from "../controllers/favoritesController.js";
+import { addToFavorites, getFavorites } from "../controllers/favoritesController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const favoriteRoutes = express.Router();
 
-favoriteRoutes.post('/:username/add/',authMiddleware, addToFavorites)
+favoriteRoutes.post('/:username/add/',authMiddleware, addToFavorites);
+favoriteRoutes.get('/:username',authMiddleware, getFavorites);
 export default favoriteRoutes;
