829159ab98a9ee040f3f8b6bf0ea9a683b1b47c7
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-02-20 19:21:15 +0530

    handle auth header missing error

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,9 +4,15 @@ import dotenv from "dotenv"
 dotenv.config();
 
 const secret = process.env.JWT_SECRET;
-export const authMiddleware = async(req, res, next) => {
+export const authMiddleware = async (req, res, next) => {
     console.log("Auth middleware called ......");
     try {
+        if (!req.headers.authorization)
+            throw {
+                "name": "AuthorizationError",
+                "message": "User is not authorized !",
+                "statusCode": 401
+            }
         const token = req.headers.authorization.split(" ")[1];
         const payload = jwt.verify(token, secret);
         // console.log(token);
@@ -16,4 +22,4 @@ export const authMiddleware = async(req, res, next) => {
     } catch (error) {
         next(error);
     }
-}
+} 
