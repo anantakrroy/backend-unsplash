1cdf29201ced77fd76f387725b21656ac52f70d3
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-02-20 19:21:57 +0530

    Allow user to add only to their collection

--- controllers/favoritesController.js
+++ controllers/favoritesController.js
@@ -8,8 +8,17 @@ import { addFavorites } from "../models/favoritePhotoModel.js";
 // Add to favorites collection
 export const addToFavorites = asyncHandler(async (req, res, next) => {
     console.log("Add to favs controller called ......");
+    const username = req.params.username;
+    const userLoggingIn = req.data.username;
+    if(username !== userLoggingIn) {
+        throw {
+            "name" : "AuthorizationError",
+            "message" : "Unauthenticated user ! You can add photos to your own collection !",
+            "statusCode" : 401
+        }
+    }
     const favPhoto = req.body;
-    const data = await addFavorites(favPhoto);
+    const data = await addFavorites(favPhoto, username);
     res.status(200).json({
         "message" : "Photo added to favorites !",
         "collection" : data
