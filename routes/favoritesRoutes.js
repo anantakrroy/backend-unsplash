281581d0f22a109de0630c276ea67e7c4e458274
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-02-21 14:12:06 +0530

    Added route to remove favorite photo

--- routes/favoritesRoutes.js
+++ routes/favoritesRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addToFavorites, getFavorites } from "../controllers/favoritesController.js";
+import { addToFavorites, getFavorites, removeFavorite } from "../controllers/favoritesController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const favoriteRoutes = express.Router();
 
-favoriteRoutes.post('/:username/add/',authMiddleware, addToFavorites);
+favoriteRoutes.post('/:username/add',authMiddleware, addToFavorites);
 favoriteRoutes.get('/:username',authMiddleware, getFavorites);
+favoriteRoutes.get('/:username/delete',authMiddleware, removeFavorite);
 export default favoriteRoutes;
