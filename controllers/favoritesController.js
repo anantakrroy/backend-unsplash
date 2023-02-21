adcd3171a783d1b89e081de6d796f96310fd16e2
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-02-21 15:11:43 +0530

    Add edit photo controller

--- controllers/favoritesController.js
+++ controllers/favoritesController.js
@@ -1,7 +1,7 @@
 //Import asyncHandler so that we can use it in our routes to trigger error handling middleware
 import asyncHandler from "express-async-handler";
 import express from "express";
-import { addFavorites, getFavoritePhotos, removeFavoritePhoto } from "../models/favoritePhotoModel.js";
+import { addFavorites, editPhotoDescription, getFavoritePhotos, removeFavoritePhoto } from "../models/favoritePhotoModel.js";
 
 
 
@@ -29,4 +29,15 @@ export const removeFavorite = asyncHandler(async (req,res, next) => {
     const photo = req.body;
     const toDelete = await removeFavoritePhoto(photo);
     res.status(200).json(toDelete);
+})
+
+// Edit description of photo
+export const editDescription = asyncHandler(async(req, res) => {
+    const photoId = req.body.id;
+    const newDescription = req.body.description;
+    const newExplanation = req.body.explanation;
+    const editDesc = await editPhotoDescription(photoId, newDescription, newExplanation);
+    res.status(200).json({
+        "message" : `Photo ID : ${photoId} description updated successfully !`
+    })
 })
